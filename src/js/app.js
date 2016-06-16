c70fb92b66326e2797d53d670c36a4ebe9d4066c
wmoulin <moulin.wilfried@gmail.com>
2016-06-16 17:00:25 +0200

    revue

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,10 @@
 "use strict";
 
 var threerest = require('threerest');
-var Twitter = require('twitter');
 
-import * as ServiceTwitter from "./services/serviceTwitter";
+import ServiceTwitter from "./services/serviceTwitter";
 
 import express from "express";
-
-
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
-
 
 var app = express();
 
@@ -22,17 +12,7 @@ app.get("/", function(req, res){
   res.send("hello world");
 });
 
-var params = {q: '@SQLI_ENTERPRISE'};
-client.get('search/tweets', params, function(error, tweets, response){
-  console.log("Hi");
-  console.log(error);
-  if (!error) {
-     console.log(tweets);
-    return tweets;
-  }
-});
-
 // load the service Test
-threerest.ServiceLoader.loadService(app, new ServiceTwitter.default(client));
+threerest.ServiceLoader.loadService(app, new ServiceTwitter());
 
 app.listen(8080, () => {console.log("Express start...");});
